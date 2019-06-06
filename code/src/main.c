e79aae92dede38876df89cd5ac1cb9cae4110897
Wilton Rodrigues <wilton.rodrigues@4linux.com.br>
2019-06-06 15:10:36 -0300

    Finalizando insert dos dados no BD

--- code/src/main.c
+++ code/src/main.c
@@ -1,5 +1,6 @@
 #include "mysql_utils.h"
 #include "parser_options.h"
+#include <string.h>
 
 int main(int argc, char* argv[])
 {
@@ -7,22 +8,39 @@ int main(int argc, char* argv[])
   parser_options(argc, argv);
 
   MYSQL *conn = mysql_init(NULL);
-  /* const char* query_so = NULL; */
-  /* const char* query_software = NULL; */
+  char query_so[500];
+  char query_software[300];
 
-  /* if (con == NULL) */
-  /* { */
-  /*   fprintf(stderr, "%s\n", mysql_error(con)); */
-  /*   exit(1); */
-  /* } */
-  /*  */
-  /* if (mysql_real_connect(con, db_host, db_user, db_passwd, */
-  /*       db_database, 0, NULL, 0) == NULL) */
-  /* { */
-  /*   finish_with_error(con); */
-  /* } */
+  sprintf(query_so, "INSERT INTO %s (hostname,tipo_equipamento,ip,so,kernel,baixa_imagem,data) "
+      "VALUES ('%s','%s','%s','%s','%s','%s','%s') "
+      "ON DUPLICATE KEY UPDATE ip=VALUES(ip),so=VALUES(so),kernel=VALUES(kernel),"
+      "baixa_imagem=VALUES(baixa_imagem),data=VALUES(data)",
+      db_so_table, hostname, tipo_equipamento, endereco_ip, sistema_operacional,
+      kernel, baixa_imagem, data_inventario);
 
-  printf("Perimeter: %s\n",db_host);
+  sprintf(query_software, "INSERT INTO %s (hostname,cmthin,fabricante) "
+      "VALUES ('%s','%s','%s') "
+      "ON DUPLICATE KEY UPDATE cmthin=VALUES(cmthin),fabricante=VALUES(fabricante)",
+      db_software_table, hostname, cmthin, fabricante);
+
+  if (mysql_real_connect(conn, db_host, db_user, db_passwd,
+        db_database, 0, NULL, 0) == NULL) {
+    finish_with_error(conn);
+  }
+
+  if (mysql_query(conn, query_so)) {
+    finish_with_error(conn);
+  }
+
+  if (mysql_query(conn, query_software)) {
+    finish_with_error(conn);
+  }
+
+  if (conn == NULL) {
+    fprintf(stderr, "%s\n", mysql_error(conn));
+    exit(1);
+  }
+
 
   return 0;
 }
